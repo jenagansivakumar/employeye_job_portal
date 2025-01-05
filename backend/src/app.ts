f860c92512e8032c3d9a49f43281a62b0a7fc9bf
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2025-01-05 18:34:23 +0100

    checking errors in frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import router from "./routes/routes.js"
 import { PrismaClient } from "@prisma/client"
+import cors from "cors"
 
 
 
@@ -9,6 +10,7 @@ const app = express()
 const port = 4000
 export const prisma = new PrismaClient()
 app.use(express.json())
+app.use(cors())
 app.use("/", router )
 
 
