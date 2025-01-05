c775d915a0d7fb84c7ff383ebe4c104f2397f93a
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2025-01-05 08:05:19 +0100

    starting again

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,1 @@
 import express from "express"
-import router from "./routes/routes.js"
-
-
-
-
-const app = express()
-const port = 3000
-
-
-
-app.use("/", router)
-
-app.listen(port, ()=> {
-    return console.log(`Server is running on port ${port}`)
-})
