d91bc303a59cb112c587a10809a07bc5386324fa
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2025-01-11 14:30:34 +0100

    addedd update job details

---
+++ backend/src/controllers.ts/job/deleteJob.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { findJobById } from "./findJobById.js";
+import { prisma } from "../../utils/init.js";
+
+
+
+
+
+export const deleteJob = async (req: Request, res: Response) => {
+    console.info("Hit endpoint")
+    const {id} = req.body
+    if (!id){
+        return res.status(400).json({message: "ID is required"})
+    }
+
+    const jobExists = await findJobById(id)
+    if (!jobExists){
+        return res.status(404).json({message: "Job doesn't exist"})
+    }
+
+    try {
+        const deletedJob = await prisma.jobDetail.delete({
+            where: {id: Number(id)}
+        })
+        res.status(200).json(`Deleted job id: ${id}`)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({error: "Cannot delete user"})
+    }
+}
--- backend/src/routes/jobRoutes.ts
+++ backend/src/routes/jobRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { createJob } from "../controllers.ts/job/createJob.js";
 import { fetchAllJobs } from "../controllers.ts/job/getAllJobs.js";
 import { updateJobDetail } from "../controllers.ts/job/updateJobDetails.js";
+import { deleteJob } from "../controllers.ts/job/deleteJob.js";
 
 
 
@@ -11,3 +12,4 @@ export const jobRouter = Router()
 jobRouter.post("/create", createJob)
 jobRouter.get("/fetch", fetchAllJobs)
 jobRouter.put("/update", updateJobDetail)
+jobRouter.delete("/delete", deleteJob)
