39503f6837bb0fef65bbc071698e185911f21398
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2025-01-11 09:49:32 +0100

    added error handling to deleteUser

--- backend/src/controllers.ts/deleteUser.ts
+++ backend/src/controllers.ts/deleteUser.ts
@@ -12,4 +12,18 @@ const deleteUser = async(req: Request, res: Response) => {
     const userExists = await prisma.userDetails.findUnique({
         where: {id: Number(id)}
     })
+
+    if (!userExists){
+        return res.status(404).json({message: "User does not exist"})
+    }
+
+    try {
+        const deletedUser = await prisma.userDetails.delete({
+            where: {id: Number(id)}
+        })
+        res.status(200).json(`User with ID ${id} has been deleted`)
+    } catch (error){
+        console.error(error.message)
+        res.status(500).json({error: "Cannot delete user"})
+    }
 }
