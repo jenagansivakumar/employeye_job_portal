100355170ac23e42fe1556c79cf6ecf8551590f6
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2025-01-11 10:14:26 +0100

    working on go routine fetching

--- go/crud/fetchUsers.go
+++ go/crud/fetchUsers.go
@@ -4,13 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/jenagansivakumar/api/models"
 )
 
-func handleApiFetch(errorChan chan<- error, userId string, client *http.Client) {
-
-	url := fmt.Sprintf("https://jsonplaceholder.typicode.com/user/%s", userId)
+func handleApiFetch(errorChan chan<- error, userDetailChan chan<- models.User, userId int, client *http.Client, wg *sync.WaitGroup) {
+	defer wg.Done()
+	url := fmt.Sprintf("https://jsonplaceholder.typicode.com/user/%d", userId)
 	if url == "" {
 		errorChan <- fmt.Errorf("url is empty")
 		return
@@ -23,10 +24,15 @@ func handleApiFetch(errorChan chan<- error, userId string, client *http.Client) 
 	}
 	defer resp.Body.Close()
 
-	var user []models.User
+	var users []models.User
 
-	if err := json.NewDecoder(resp.Body).Decode(&user); err != nil {
-		errorChan <- fmt.Errorf()
+	if err := json.NewDecoder(resp.Body).Decode(&users); err != nil {
+		errorChan <- fmt.Errorf("cannot decode response body")
+		return
+	}
+
+	for _, user := range users {
+		userDetailChan <- user
 	}
 
 }
@@ -36,6 +42,42 @@ func FetchUser(w http.ResponseWriter, r *http.Request, client *http.Client) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method has to be GET", http.StatusMethodNotAllowed)
 		return
+	}
+
+	var wg sync.WaitGroup
+	errorChan := make(chan error, 1)
+	userDetailChan := make(chan models.User, 1)
+
+	workers := 10
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go handleApiFetch(errorChan, userDetailChan, i, client, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(errorChan)
+		close(userDetailChan)
+	}()
+
+	var errorList []error
+	var userList []models.User
+	for {
+		select {
+		case err, ok := <-errorChan:
+			if ok {
+				errorList = append(errorList, err)
+			} else {
+				errorChan = nil
+			}
+		case userDetail, ok := <-userDetailChan:
+			if ok {
+				userList = append(userList, userDetail)
+			} else {
+
+			}
+		}
 	}
 
 }
