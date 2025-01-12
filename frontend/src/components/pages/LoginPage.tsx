173c115d109f70a141b484691610095c96fe4b3c
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2025-01-12 13:06:17 +0100

    creating login page

--- frontend/src/components/pages/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage.tsx
@@ -1,11 +1,33 @@
 import React, { useState, useTransition } from 'react'
+import {useForm} from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod'
+import { formSchema } from '../../models/FormSchema'
 
 export const LoginPage = () => {
 
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>({
+        resolver: zodResolver(formSchema)
+    })
+
     const [username, setUsername] = useState<string>("")
     const [password, setPassword] = useState<string>("")
+    const [error, setError] = useState<string>("")
+    const [loading, setLoading] = useState<boolean>(false)
+
+    const userLogin = async()=>{
+        setError("")
+        setLoading(true)
+
+
+        try {
+
+        }
+    }
     return (
-        <div>LoginPage</div>
+        <form>
+            <label>Username</label>
+                <input/>
+        </form>
 
     )
   
